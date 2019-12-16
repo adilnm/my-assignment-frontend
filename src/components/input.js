969b416728cc7d69ab5bc1e7e5c93fb76ac00e1b
Adil Naimi <adilnm23@gmail.com>
2019-12-15 22:01:54 -0600

    add ids to all the diferent inputs

--- src/components/input.js
+++ src/components/input.js
@@ -10,20 +10,27 @@ class Input {
       inputBox.appendChild(nameInput)
       nameInput.type="text"
       nameInput.placeholder="Name"
+      nameInput.id='name'
+      inputBox.innerHTML+='<br>'
 
       const categoryInput=document.createElement('input')
       inputBox.appendChild(categoryInput)
       categoryInput.type="text"
       categoryInput.placeholder="Category"
+      categoryInput.id='category'
+      inputBox.innerHTML+='<br>'
 
       const descriptionInput=document.createElement('textarea')
       inputBox.appendChild(descriptionInput)
       descriptionInput.placeholder="Description"
+      descriptionInput.id='description'
+      inputBox.innerHTML+='<br>'
 
-      // const nameInput=document.createElement('input')
-      // inputBox.appendChild(nameInput)
-      // nameInput.type="text"
-      // nameInput.placeholder="Name"
+      const gradeInput=document.createElement('input')
+      inputBox.appendChild(gradeInput)
+      gradeInput.type="text"
+      gradeInput.id='grade'
+      gradeInput.placeholder="Grade"
 
 
 
